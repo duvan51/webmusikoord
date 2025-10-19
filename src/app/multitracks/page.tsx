8ca72024d58ac18dcccc34ced3d63fea06f7b041
duvan51 <duvanaponteramirez@gmail.com>
2025-10-19 04:55:53 -0500

    arreglado cositas

---
+++ src/app/multitracks/page.tsx
@@ -0,0 +1,9 @@
+import MultiTrackPlayer from "@/components/MultitrackPlayers/MultiTrackPlayer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <MultiTrackPlayer />
+    </main>
+  );
+}
